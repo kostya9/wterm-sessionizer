e677bb8f1f4a37bc2392784f2ef56754b5313f60
Kostiantyn Sharovarskyi <ksha@jooble.com>
2023-11-29 12:00:13 +0200

    Clean up

--- src/init.ps1
+++ src/init.ps1
@@ -1,12 +1,10 @@
-
 $function:prompt = & {
 	$__last_prompt = $function:prompt
-		$count = 0
-		{ & $script:__last_prompt
-			$newDir = Get-Location
-			if ($newDir -ne $global:oldDir) {wts on-changed-directory $newDir}
-			$global:oldDir = $newDir
-		}.GetNewClosure()
+	{ & $script:__last_prompt
+		$newDir = Get-Location
+		if ($newDir -ne $global:oldDir) {wts on-changed-directory $newDir}
+		$global:oldDir = $newDir
+	}.GetNewClosure()
 }
 
 function wts { $result = @((wts.exe $args) -join "`n"); if ($result.StartsWith("<#Execute#>")) { @($result) | Invoke-Expression } else { echo $result } }
