fef73279043d160f0d5a6f936cc0244ca2ed0189
Kostiantyn Sharovarskyi <ksha@jooble.com>
2023-11-29 11:49:44 +0200

    Fix breaking posh prompt

--- src/init.ps1
+++ src/init.ps1
@@ -1,8 +1,12 @@
 
-$oldPrompt = (Get-Command prompt).ScriptBlock 
-
-$newPrompt = "$oldPrompt; `$newDir = Get-Location; if (`$newDir -ne `$oldDir) {wts on-changed-directory}; `$oldDir = `$newDir;"
-
-Set-Item -Path Function:prompt -Value $newPrompt
+$function:prompt = & {
+	$__last_prompt = $function:prompt
+		$count = 0
+		{ & $script:__last_prompt
+			$newDir = Get-Location
+			if ($newDir -ne $global:oldDir) {wts on-changed-directory $newDir}
+			$global:oldDir = $newDir
+		}.GetNewClosure()
+}
 
 function wts { $result = @((wts.exe $args) -join "`n"); if ($result.StartsWith("<#Execute#>")) { @($result) | Invoke-Expression } else { echo $result } }
